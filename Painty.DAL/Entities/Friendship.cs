9ac32b71ed2417dc58e5c26f336a1d7aa14006b8
AlexanderLukashuk <upiter465@gmail.com>
2023-10-18 16:00:36 +0600

    Added Id property in Frindship class

--- Painty.DAL/Entities/Friendship.cs
+++ Painty.DAL/Entities/Friendship.cs
@@ -3,6 +3,8 @@ namespace Painty.DAL.Entities
 {
 	public class Friendship
 	{
+        public int Id { get; set; }
+
         public int User1Id { get; set; }
 
         public int User2Id { get; set; }
