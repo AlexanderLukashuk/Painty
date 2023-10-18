5f8f60c60b0fd3a315a7f3cbd3b07669ce089f3e
AlexanderLukashuk <upiter465@gmail.com>
2023-10-18 07:50:56 +0600

    Created ImageRepository

---
+++ Painty.DAL/Repositories/ImageRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Painty.DAL.EF;
+using Painty.DAL.Entities;
+using Painty.DAL.Interfaces;
+
+namespace Painty.DAL.Repositories
+{
+	public class ImageRepository : IRepository<Image>
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ImageRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+        public async Task Add(Image enitity)
+        {
+            await _context.Images.AddAsync(enitity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var image = await GetById(id);
+            if (image != null)
+            {
+                _context.Images.Remove(image);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public IEnumerable<Image> GetAll()
+        {
+            return _context.Images;
+        }
+
+        public async Task<Image> GetById(int id)
+        {
+            return await _context.Images.FindAsync(id);
+        }
+
+        public async Task<Image> Update(Image entity)
+        {
+            _context.Images.Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
+
