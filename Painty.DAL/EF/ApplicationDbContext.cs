22afc20b10f58930a56db67fd1dc678f402b0f53
AlexanderLukashuk <upiter465@gmail.com>
2023-10-18 07:25:59 +0600

    Createad ApplicationDbContext

---
+++ Painty.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Painty.DAL.Entities;
+
+namespace Painty.DAL.EF
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+		public DbSet<Image> Images { get; set; }
+
+		public DbSet<Friendship> Friendships { get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<User>()
+				.HasMany(u => u.Images)
+				.WithOne()
+				.HasForeignKey(i => i.UserId);
+        }
+    }
+}
+
