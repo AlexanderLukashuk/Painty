67b37b17ece8699700cd3eb34cda18eba867241e
AlexanderLukashuk <upiter465@gmail.com>
2023-10-18 07:32:14 +0600

    Created interface for repositories

---
+++ Painty.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Painty.DAL.Interfaces
+{
+	public interface IRepository<T> where T : class
+	{
+		T GetById(int id);
+
+		IEnumerable<T> GetAll();
+
+		Task Add(T enitity);
+
+		Task Delete(int id);
+
+		Task<T> Update(T entity);
+	}
+}
+
